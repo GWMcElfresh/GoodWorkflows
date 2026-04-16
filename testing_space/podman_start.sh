655dca31f504abf6436100e69beac96b9eb2da6f
GW McElfresh <mcelfreshgw@gmail.com>
2026-04-16 14:39:53 -0700

    Delete testing_space directory

--- testing_space/podman_start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-
-podman machine init
-podman machine set --memory 12288 podman-machine-default && \
-podman machine start podman-machine-default 
-
-#podman system info 2>/dev/null | grep -E 'memTotal|memFree'
